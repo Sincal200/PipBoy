ba3a5747f1b97ab448146c5a8b79b3242bb4bd2e
Sincal200 <sincaljulio200@gmail.com>
2024-10-04 23:35:20 -0600

    Test Conexion 2

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,8 @@
-// index.js
 const express = require('express');
 const { mongoose } = require('mongoose');
 const dotenv = require('dotenv').config();
-const http = require('http'); // Importar http
-const socketIo = require('socket.io'); // Importar socket.io
+const http = require('http');
+const socketIo = require('socket.io');
 
 const app = express();
 
@@ -16,35 +15,28 @@ app.use('/', require('./routes/dataRoutes'));
 
 const port = 3000;
 
-// Crear servidor HTTP
 const server = http.createServer(app);
 
-// Inicializar socket.io con el servidor HTTP
 const io = socketIo(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: '*', // Permitir todas las conexiones
     methods: ['GET', 'POST']
   }
 });
 
-// Configurar eventos de conexión de WebSocket
 io.on('connection', (socket) => {
   console.log('New client connected');
 
-  // Ejemplo de evento de mensaje
   socket.on('message', (data) => {
     console.log('Message received:', data);
-    // Emitir mensaje a todos los clientes conectados
     io.emit('message', data);
   });
 
-  // Evento de desconexión
   socket.on('disconnect', () => {
     console.log('Client disconnected');
   });
 });
 
-// Usar el servidor HTTP para escuchar en el puerto
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
