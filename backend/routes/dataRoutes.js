90dd3005d670db9b8209fe47a81e41ed8d237bfb
Sincal200 <sincaljulio200@gmail.com>
2024-10-08 19:27:51 -0600

    PB-13 <Cambios CORS para pruebas>

--- backend/routes/dataRoutes.js
+++ backend/routes/dataRoutes.js
@@ -3,10 +3,19 @@ const router = express.Router();
 const cors = require('cors');
 const { create, test, getAll, startTemperature, stopTemperature, startSensorData, stopSensorData, sensorData} = require('../controllers/dataController');
 
-
+const allowedOrigins = [
+  'https://pipboy-frontend.onrender.com',
+  'http://localhost:5173',
+];
 
 const corsOptions = {
-  origin: 'https://pipboy-frontend.onrender.com',
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   allowedHeaders: 'Content-Type,Authorization'
