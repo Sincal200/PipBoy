b7f40ea17300ab34a1bc86a9687ceb56a6cc3d22
Sincal200 <sincaljulio200@gmail.com>
2024-10-11 17:17:53 -0600

    Chale

--- frontend/pipBoy/vite.config.js
+++ frontend/pipBoy/vite.config.js
@@ -1,10 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({
-    // Add this line
-    include: "**/*.tsx",
-  })]
-})
+  plugins: [react()],
+  server: {
+    historyApiFallback: true, // Añade esta línea
+  },
+});
