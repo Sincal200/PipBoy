53afcbb85571aa1dfe4ff56d4c1b3d78b5ca41ac
Sincal200 <sincaljulio200@gmail.com>
2024-10-17 17:38:49 -0600

    T

--- backend/middlewares/addUserMiddleware.js
+++ backend/middlewares/addUserMiddleware.js
@@ -1,11 +1,11 @@
 const addUserMiddleware = (req, res, next) => {
-  const user = req.session.userName; // Obtener el nombre del usuario desde la sesión
-  if (user) {
-    req.body.user = user; // Añadir el nombre del usuario al cuerpo de la solicitud
-  } else {
-    req.body.user = 'usuario_hardcoded'; // Valor por defecto si no se encuentra el nombre del usuario
-  }
-  next();
-};
-
-module.exports = addUserMiddleware;
+    const userName = req.session.userName; // Obtener el nombre del usuario desde la sesión
+    if (userName) {
+      req.body.user = userName; // Añadir el nombre del usuario al cuerpo de la solicitud
+    } else {
+      req.body.user = 'usuario_hardcoded'; // Valor por defecto si no se encuentra el nombre del usuario
+    }
+    next();
+  };
+  
+  module.exports = addUserMiddleware;
